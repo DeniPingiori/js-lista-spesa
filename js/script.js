97c70bd22d95cc4495c770f5bf321789ea0c3a89
Denise Pingiori <pingiorid@gmail.com>
2024-07-06 11:44:20 +0200

    [upd] finished script js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ const lista_spesa = ['guanciale', 'uova', 'pasta', 'pecorino', 'pepe nero' ];
 
 // recupero elemento che deve contenere i valori dell'array
 const ul = document.getElementById('lista-spesa'); 
+
+//ciclo l'array con il while
+
+//dichiaro var contatore
+let i = 0; 
+
+//definisco il while con condizione di fine ciclo
+while (i<lista_spesa.length){
+
+    // creo un nuovo list item
+    const li = document.createElement('li');
+
+    //inserisco il testo all'interno, innesto lista spesa in posizione i 
+    li.innerText = lista_spesa[i]; 
+
+    //aggiungo il list item alla lista
+    ul.append(li);
+
+    //instruzione che fa terminare il ciclo
+    i++;
+
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,11 @@ Buon lavoro!
 scomposizione es: 
 1. - dichiaro costante lista della spesa con array
 2. - recupero elemento che deve contenere i valori dell'array
+3. - ciclo l'array con il while
+    3.1 - dichiaro var contatore
+    3.2 - definisco il while con condizione di fine ciclo
+    3.3 - creo un nuovo list item
+    3.4 - inserisco il testo all'interno, innesto lista spesa in posizione i 
+    3.5 - aggiungo il list item alla lista
+4. instruzione che fa terminare il ciclo
+
